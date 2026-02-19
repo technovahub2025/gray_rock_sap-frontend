7d75c6b50c3c565ce91d056b6c265c3e21e0f3b4
Technovahub Career <technovahubcareer@gmail.com>
2026-02-19 18:34:17 +0530

    vite config live url updated

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': env.VITE_API_PROXY_TARGET || 'http://localhost:5000',
-      },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://grayrock-backend.onrender.com',
     },
-  }
+  },
 })
